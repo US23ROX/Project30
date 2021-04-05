f48733d212a7a501e1d380e7878703065c5b2088
US23ROX <76163180+US23ROX@users.noreply.github.com>
2021-04-05 16:33:58 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,29 @@
+class Block{
+  constructor(x, y, width, height) {
+    this.image = loadImage("block.png");
+    this.visibility = 255;
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+
+      console.log(this.body.speed);
+      if(this.body.speed>3){
+      World.remove(world,this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      tint(255,this.visibility);
+      image(this.image,this.body.position.x,this.body.position.y);
+      pop();
+    }
+  }
+}
